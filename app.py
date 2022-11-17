bc489d310bc5b809455fcf8e66256962c3c89727
Jenna Mui <Jenna.mui@stonybrook.edu>
2022-11-17 01:43:36 -0500

    update

--- app.py
+++ app.py
@@ -3,16 +3,18 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title('Hospital Inpatient Discharges 2016')
 
-DATE_COLUMN = 'date/time'
+st.title('Hospital Inpatient Discharge Dataframes')
+
+st.write('These are 2 dataframes with patient discharge information.')
+
+st.header('This dataframe 1')
 
 @st.cache
 def load_data(nrows):
-    data = pd.read_csv('/Users/jennamui/Documents/GitHub/dashboard-streamlit/Hospital_Inpatient_Discharges__SPARCS_De-Identified___2016.csv', nrows=nrows)
+    data = pd.read_csv('https://raw.githubusercontent.com/hantswilliams/HHA-507-2022/main/autoML/datasets/data_sparcs.csv', nrows=nrows)
     lowercase = lambda x: str(x).lower()
     data.rename(lowercase, axis='columns', inplace=True)
-    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
     return data
 
 # Create a text element and let the reader know the data is loading.
@@ -23,11 +25,41 @@ data = load_data(10000)
 data_load_state.text("Done! (using st.cache)")
 
 if st.checkbox('Show raw data'):
-    st.subheader('Raw data')
+    st.subheader('Dataframe 1')
     st.write(data)
 
+
+st.subheader('Bar Graph')
 chart_data = pd.DataFrame(
-    np.random.randn(50, 3),
-    columns=['Self-Pay','Medicare','Medicaid','Private Insurance','Blue Cross/Blue Shield'])
+    np.random.randn(10, 5),
+    columns=['0-17','18-29','30-49','50-69','70+'])
 
 st.bar_chart(chart_data)
+
+
+st.header('This is dataframe 2')
+
+@st.cache
+def load_data(nrows):
+    data2 = pd.read_csv('https://health.data.ny.gov/resource/gnzp-ekau.csv', nrows=nrows)
+    lowercase2 = lambda x: str(x).lower()
+    data2.rename(lowercase2, axis='columns', inplace=True)
+    return data2
+
+# Create a text element and let the reader know the data is loading.
+data2_load_state = st.text('Loading data...')
+# Load 10,000 rows of data into the dataframe.
+data2 = load_data(10000)
+# Notify the reader that the data was successfully loaded.
+data2_load_state.text("Done! (using st.cache)")
+
+if st.checkbox('Show dataframe'):
+    st.subheader('Dataframe 2')
+    st.write(data2)
+
+st.subheader('Line Graph')
+chart_data2 = pd.DataFrame(
+    np.random.randn(20, 4),
+    columns=['Emergency', 'Elective', 'Newborn','Urgent'])
+
+st.line_chart(chart_data2)
